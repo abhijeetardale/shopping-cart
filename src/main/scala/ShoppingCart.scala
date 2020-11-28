603f4a80bb5be9a96236235f179ce2b124c68a81
“abhijeetardale” <26648207+abhijeetardale@users.noreply.github.com>
2020-11-28 18:15:15 +0000

    test passed

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -3,11 +3,11 @@ object ShoppingCart {
 	def applePrice(numberOfItems: Int) : Double = {
 
 		numberOfItems * 0.6
-
 	}
 
 	def orangePrice(numberOfItems: Int) : Double = {
-		0.25
+
+		numberOfItems * 0.25
 	}
 
 }

02f4b35024954c2593d8f8548ddd117d61db4823
“abhijeetardale” <26648207+abhijeetardale@users.noreply.github.com>
2020-11-28 19:03:31 +0000

    refactoring of tests

--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -2,55 +2,55 @@ import org.scalatest.{MustMatchers, WordSpec}
 
 class ShoppingCartSpec extends WordSpec with MustMatchers {
 
-	val cart = ShoppingCart
+	private val cart = ShoppingCart
 
 	"ShoppingCart" when {
 
 		"Calling apple price" must {
 
 			"return £0.6 for an Apple" in {
-				cart.applePrice(1, false) mustBe 0.6
+				cart.applePrice(1, offer = false) mustBe 0.6
 			}
 
 			"return £1.2 for two Apples" in {
-				cart.applePrice(2, false) mustBe 1.2
+				cart.applePrice(2, offer = false) mustBe 1.2
 			}
 
 			"return £0.6 for 2 Apples with an offer" in {
-				cart.applePrice(2, true) mustBe 0.6
+				cart.applePrice(2, offer = true) mustBe 0.6
 			}
 
 			"rerun £1.2 for 3 Apples with an offer" in {
-				cart.applePrice(3, true) mustBe 1.2
+				cart.applePrice(3, offer = true) mustBe 1.2
 			}
 
 			"return £2.8 for list 0f Apple, Apple, Orange, Orange, Orange, Apple, Apple if no offer" in {
 
-				cart.checkout(List("Orange", "Apple", "Apple", "Orange", "Orange", "Orange", "Apple"), false) mustBe "£2.8"
+				cart.checkout(List("Orange", "Apple", "Apple", "Orange", "Orange", "Orange", "Apple"), applyOffer = false) mustBe "£2.8"
 			}
 
 			"return £2.8 for list 0f Apple, Apple, Orange, Orange, Orange, Apple, Apple if offer applied" in {
 
-				ShoppingCart.checkout(List("Orange", "Apple", "Apple", "Orange", "Orange", "Orange", "Apple"), true) mustBe "£1.95"
+				cart.checkout(List("Orange", "Apple", "Apple", "Orange", "Orange", "Orange", "Apple"), applyOffer = true) mustBe "£1.95"
 			}
 		}
 
 		"Calling orange price" must {
 
 			"return £0.25 for an Orange" in {
-				cart.orangePrice(1, false) mustBe 0.25
+				cart.orangePrice(1, offer = false) mustBe 0.25
 			}
 
 			"return £0.25 for two Oranges" in {
-				cart.orangePrice(2, false) mustBe 0.50
+				cart.orangePrice(2, offer = false) mustBe 0.50
 			}
 
 			"rerun £0.50 for three Oranges with an offer" in {
-				cart.orangePrice(3, true) mustBe 0.50
+				cart.orangePrice(3, offer = true) mustBe 0.50
 			}
 
 			"rerun £0.75 for 4 Oranges" in {
-				cart.orangePrice(4, true) mustBe 0.75
+				cart.orangePrice(4, offer = true) mustBe 0.75
 			}
 		}
 
@@ -58,22 +58,22 @@ class ShoppingCartSpec extends WordSpec with MustMatchers {
 
 			"return 0 if cart is empty" in {
 
-				cart.checkout(List.empty, false) mustBe "£0.0"
+				cart.checkout(List.empty, applyOffer = false) mustBe "£0.0"
 			}
 
 			"return £1.2 if cart contains 2 Apples" in {
 
-				cart.checkout(List("Apple", "Apple"), false) mustBe "£1.2"
+				cart.checkout(List("Apple", "Apple"), applyOffer = false) mustBe "£1.2"
 			}
 
 			"return £1.0 if cart contains 4 Oranges" in {
 
-				cart.checkout(List("Orange", "Orange", "Orange", "Orange"), false) mustBe "£1.0"
+				cart.checkout(List("Orange", "Orange", "Orange", "Orange"), applyOffer = false) mustBe "£1.0"
 			}
 
 			"return £2.05 for list 0f Apple, Apple, Orange, Apple" in {
 
-				cart.checkout(List("Apple", "Apple", "Orange", "Apple"), false) mustBe "£2.05"
+				cart.checkout(List("Apple", "Apple", "Orange", "Apple"), applyOffer = false) mustBe "£2.05"
 			}
 		}
 	}
